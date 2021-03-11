7847174f12f8defa7c8c7495ff307e6234e56bc0
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-03-11 20:12:13 +0530

    vince: add props to make blur smoother
    blur lags  on vince, so added some props to make it smoother
    
    Signed-off-by: alanmathew33 <alanmadathil007@gmail.com>

--- prop.mk
+++ prop.mk
@@ -60,8 +60,10 @@ ro.bluetooth.hfp.ver=1.7
 
 # Blur
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
+    ro.sf.blurs_are_expensive=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
 
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
