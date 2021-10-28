d76a5caf0f5fa71b1f05b5111e3bda94899564db
XSJoJo <>
2021-10-28 21:35:12 +0530

    vince: props: Disable sdm rotator downscaler
    Rotator down scaler is causing additional delays that are better
    handled by GL composition.
    
    Test: Open app, no jank
    Bug: 73640835
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -169,6 +169,7 @@ ro.qualcomm.cabl=0 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
+vendor.display.disable_rotator_downscale=1 \
 vendor.display.enable_default_color_mode=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
 video.accelerate.hw=1
