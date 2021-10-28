1e445098b028e3dfcfaa57d8912e33035dfe55d6
anandhan07 <anandhasayanan007jb@gmail.com>
2021-10-28 21:37:25 +0530

    vince: props: Disable blur
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -108,10 +108,7 @@ ro.bluetooth.hfp.ver=1.7
 
 # Blur
 PRODUCT_PRODUCT_PROPERTIES += \
-ro.surface_flinger.supports_background_blur=1 \
-ro.sf.blurs_are_expensive=1 \
-debug.sf.latch_unsignaled=1 \
-debug.sf.disable_backpressure=1
+persist.sys.sf.disable_blurs=1
 
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
