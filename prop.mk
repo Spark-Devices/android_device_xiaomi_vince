24c2e19a5d3b180fcbb42d6bbf5a162dce357fec
Randall Huang <huangrandall@google.com>
2021-10-28 19:18:27 +0530

    vince: Enable zram-writeback job
    This patch enables zram-writeback job. It will run idle marking
    at 60m since the device is boot. After that, it will do first
    writeback after 3hour to sweep initiallized cold pages out.
    After that, it periodically write out every 24hour.
    
    Bug: 117682284
    Bug: 127185055
    Test: confirm with manual testing
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -310,3 +310,9 @@ wifi.interface=wlan0
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.wfd.enable=1 \
 persist.sys.wfd.virtual=0
+
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.zram.mark_idle_delay_mins=60 \
+ro.zram.first_wb_delay_mins=180 \
+ro.zram.periodic_wb_delay_hours=24
