34aa785067fe84866ca0531f0e0108d3b9f58a34
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 17:56:04 +0530

    vince: Build camera HAL from source
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -154,6 +154,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.frameworks.displayservice@1.0.vendor \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
     libdng_sdk.vendor
