66bb63d76e3ab7a0d0c69aea34120e19a7072106
erfanoabdi <erfangplus@gmail.com>
2022-01-18 09:15:45 +0530

    vince: prepare to move to in-tree HALs
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -43,6 +43,9 @@ PRODUCT_BOARD_PLATFORM := msm8953
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
