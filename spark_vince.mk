efbcb9bbd9492ce8965b405e3d83dea34e4bcf21
anandhan07 <anandhasayanan007jb@gmail.com>
2022-02-12 18:09:33 +0530

    vince: update to february 2022 raven fp
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- spark_vince.mk
+++ spark_vince.mk
@@ -49,4 +49,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
     
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
