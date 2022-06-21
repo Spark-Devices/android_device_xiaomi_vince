ed6efa23f6abca308c3aa72bc1c069bc5c1b97c9
Albert I <krascgq@outlook.co.id>
2022-06-21 18:42:27 +0530

    vince: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -290,6 +290,10 @@ PRODUCT_PACKAGES += \
     ims_ext_common.xml \
     libui_shim
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
