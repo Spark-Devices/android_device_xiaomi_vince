c8be3469a0fc169b7a2bcfb8754e6b0d289cefa4
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2022-07-06 14:44:23 +0530

    vince: Build libvndfwk_detect_jni.qti.vendor
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -221,7 +221,8 @@ PRODUCT_PACKAGES += \
 
 # Fwk-detect
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti.vendor
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
