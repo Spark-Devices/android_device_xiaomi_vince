d8edf114aea33379bf00f151d3c236905242bcf7
Ajay Dudani <Ajay Dudani@users.noreply.github.com>
2022-07-15 20:20:50 +0530

    vince: props: Update dalvik.vm.heapgrowthlimit to 256m
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -17,7 +17,7 @@ dalvik.vm.image-dex2oat-threads=8
 # Dalvik
 PRODUCT_VENDOR_PROPERTIES += \
 dalvik.vm.heapstartsize=16m \
-dalvik.vm.heapgrowthlimit=192m \
+dalvik.vm.heapgrowthlimit=256m \
 dalvik.vm.heapsize=512m \
 dalvik.vm.heaptargetutilization=0.75 \
 dalvik.vm.heapminfree=4m \
