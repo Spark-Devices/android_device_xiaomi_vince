2f10dd5b7c0cae2cd4481b0f8b2ad94c98cdbdc4
anandhan07 <anandhasayanan007jb@gmail.com>
2022-07-15 20:24:06 +0530

    vince: props: Set dalvik.vm.heapminfree to 512k
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -20,7 +20,7 @@ dalvik.vm.heapstartsize=16m \
 dalvik.vm.heapgrowthlimit=256m \
 dalvik.vm.heapsize=512m \
 dalvik.vm.heaptargetutilization=0.75 \
-dalvik.vm.heapminfree=4m \
+dalvik.vm.heapminfree=512k \
 dalvik.vm.heapmaxfree=8m
 
 # Audio
