ac4d1e7855fb75088ea916ce40e39dc897d7de52
anandhan07 <anandhasayanan007jb@gmail.com>
2022-08-14 10:19:03 +0530

    Revert "vince: Add dalvik props for now"
    This reverts commit 91a02ae89478a9afc96d72b133b0c1b40405a1e9.
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -14,15 +14,6 @@ dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
 dalvik.vm.image-dex2oat-filter=quicken \
 dalvik.vm.image-dex2oat-threads=8 
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapstartsize=16m \
-dalvik.vm.heapgrowthlimit=256m \
-dalvik.vm.heapsize=512m \
-dalvik.vm.heaptargetutilization=0.75 \
-dalvik.vm.heapminfree=4m \
-dalvik.vm.heapmaxfree=8m
-
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
