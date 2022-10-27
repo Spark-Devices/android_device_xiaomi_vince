5b9d92979d32f7ef34f4735f68b7aa160b708517
anandhan07 <anandhasayanan007jb@gmail.com>
2022-10-27 11:43:36 +0530

    vince: media-hal: Update Media HAL from LA.UM.10.6.2.r1-02500-89xx.0
    * From - https://github.com/anandhan07/android_hardware_qcom_media/tree/thirteen-LA.UM.10.6.2.r1-02500-89xx.0
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- qcom-caf/media/mm-video-v4l2/vidc/venc/Android.mk
+++ qcom-caf/media/mm-video-v4l2/vidc/venc/Android.mk
@@ -140,7 +140,6 @@ LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libmm-venc-def)
 
 LOCAL_HEADER_LIBRARIES := \
-        display_headers \
         media_plugin_headers \
         libnativebase_headers \
         libcutils_headers \
@@ -184,7 +183,6 @@ LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libmm-venc-def)
 
 LOCAL_HEADER_LIBRARIES := \
-        display_headers \
         media_plugin_headers \
         libnativebase_headers \
         libutils_headers \
