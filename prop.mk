4eadc15936a6c43e1c7a9f6e0672d75dcbe0ad2b
anandhan07 <anandhasayanan007jb@gmail.com>
2022-10-29 18:40:45 +0530

    vince: props: Fix bt device name space breakage
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -94,7 +94,7 @@ vendor.audio.feature.incall_music.enable=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-bluetooth.device.default_name=Redmi 5 Plus \
+bluetooth.device.default_name=Redmi-5-Plus \
 persist.vendor.service.bdroid.sibs=false \
 vendor.qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7
