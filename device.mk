a5002bfde25eec0983568b89ef0585e055ce42b7
bgcngm <bgcngm@users.noreply.github.com>
2022-11-07 11:50:18 +0530

    vince: Drop default DRM service
    * This is not needed, since Widevine DRM and Clearkey DRM services are available.
    * Clearkey and Widevine have their own vintf
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -202,9 +202,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
 # Fingerprint
