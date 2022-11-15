d48dc69e2bfff9f4482776fa1f39e55261ded558
Jun Wang <Jun Wang@users.noreply.github.com>
2022-11-15 10:39:19 +0530

    vince: props: Enable the pre-rendering feature
    * Pre-rendering feature can improve FPS and reduce the Janks in scrolling cases.
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -242,7 +242,8 @@ vendor.video.disable.ubwc=1
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sys.fw.dex2oat_thread_count=8 \
-ro.vendor.extension_library=libqti-perfd-client.so
+ro.vendor.extension_library=libqti-perfd-client.so \
+ro.vendor.perf.scroll_opt=true
 
 # Network manager daemon
 PRODUCT_PROPERTY_OVERRIDES += \
