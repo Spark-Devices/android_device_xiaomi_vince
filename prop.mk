7c97b33ed500b9f2bfed4571edb3f25f8484e4dc
johnmart19 <johnivan19999@gmail.com>
2022-12-12 18:48:32 +0530

    vince: props: Adjust Surfaceflinger refresh rate idle timer
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- prop.mk
+++ prop.mk
@@ -335,7 +335,7 @@ ro.surface_flinger.protected_contents=true \
 ro.surface_flinger.use_content_detection_for_refresh_rate=true \
 ro.surface_flinger.set_display_power_timer_ms=10000 \
 ro.surface_flinger.set_touch_timer_ms=200 \
-ro.surface_flinger.set_idle_timer_ms=9000 \
+ro.surface_flinger.set_idle_timer_ms=1000 \
 ro.surface_flinger.wcg_composition_dataspace=143261696
 
 # System
