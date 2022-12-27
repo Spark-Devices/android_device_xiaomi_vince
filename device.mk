d00d5d260d8e7d7c4d16ac913025eec8302cedd5
dev-harsh1998 <dev-harsh1998@hotmail.com>
2022-12-27 12:06:28 +0530

    Reland "vince: Strip off some debug packages"
    This reverts commit afc4eb3176c0a602542f27311f662af5f6bacb9f.
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -480,6 +480,9 @@ PRODUCT_COPY_FILES += \
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
 
 # Whitelisted apps
 PRODUCT_COPY_FILES += \
