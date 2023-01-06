1e3ab099c1c9d8537ddc07a26ac7d2043416ef04
anandhan07 <anandhasayanan007jb@gmail.com>
2023-01-06 18:52:12 +0530

    vince: Remove no-op thermal.msm8953
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -440,8 +440,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
-    android.hardware.thermal@1.0.vendor \
-    thermal.msm8953
+    android.hardware.thermal@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
