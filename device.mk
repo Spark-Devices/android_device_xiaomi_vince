539d77fe05e681fb96638433962d536158f5c4a1
gamehackerever <gamehackerever@users.noreply.github.com>
2023-01-10 09:07:45 +0530

    vince: Downgrade Perf blobs to LA.UM.8.6.2.r1-08400-89xx.0
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -339,7 +339,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
