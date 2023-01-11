acb4d44bd3137ea7d73476a8658309e3eca136de
anandhan07 <anandhasayanan007jb@gmail.com>
2023-01-11 20:17:44 +0530

    vince: Clean up removepackages list
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube Videos
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
