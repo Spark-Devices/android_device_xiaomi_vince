1eec3163b0623fa3d907490f566521045f50f998
anandhan07 <anandhasayanan007jb@gmail.com>
2023-03-21 10:43:36 +0530

    vince: Kang perf stack from LA.VENDOR.1.0.r1-16000-WAIPIO.QSSI13.0
    * Blobs from:- https://github.com/ThankYouMario/proprietary_vendor_qcom_common
    * Also import msm_irqbalance from daisy
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -270,10 +270,6 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
@@ -345,7 +341,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.1.vendor
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
