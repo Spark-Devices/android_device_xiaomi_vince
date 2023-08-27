1abd46101a4ee19a32c72f61fb9097bec5df506a
GhostMaster69-dev <rathore6375@gmail.com>
2023-08-27 19:33:17 +0530

    vince: rootdir: Disable collective Device administrators GMS components
    - Also, Disable com.google.android.gms.chimera.GmsIntentOperationService properly.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -43,4 +43,7 @@ if [ -f /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies ]; then
     setprop vendor.gpu.available_frequencies "$gpu_freq"
 fi
 
-pm disable com.google.android.gms/.chimera.GmsIntentOperationService
+# Disable GMS components
+pm disable com.google.android.gms/com.google.android.gms.auth.managed.admin.DeviceAdminReceiver
+pm disable com.google.android.gms/com.google.android.gms.chimera.GmsIntentOperationService
+pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver
